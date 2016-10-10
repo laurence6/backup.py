d9075b49a2ad771d69dab009341c4d8d563663a1
Laurence <liuxy6@gmail.com>
2016-10-09 23:04:47 -0400

    Make default_options a global variable

--- backup.py
+++ backup.py
@@ -26,44 +26,60 @@ from sys import argv, exit
 from time import time
 
 __NAME__ = basename(argv.pop(0))
-__VERSION__ = '0.7.3'
+__VERSION__ = '0.7.4'
+
+DEFAULT_OPTIONS = ' '.join([
+    '--verbose',
+    '--human-readable',
+    '--archive',
+    '--hard-links',
+    '--acls',
+    '--xattrs',
+    '--numeric-ids',
+    '--noatime',
+    '--inplace',
+    '--delete',
+    '--delete-excluded',
+])
 
 logger = logging.getLogger('main')
 
 
 def print_help():
-    print('%s %s, Use rsync to backup and to restore files.\n'
-          'Usage: %s [OPTIONS...] CONFIG_FILE [ADDITIONAL_RSYNC_OPTIONS...]\n'
-          '\n'
-          'OPTIONS:\n'
-          '\n'
-          '  Informative Output:\n'
-          '      -q, --quiet                 keep quiet\n'
-          '      -v, --verbose               increase verbosity\n'
-          '\n'
-          '  Backup Options:\n'
-          '          --rsnyc-opts="..."      replace the default rsync options\n'
-          '      -n, --show-cmd              print rsync command and exit\n'
-          '\n'
-          '  Other Options:\n'
-          '      -h, --help                  print this help list\n'
-          '      -V, --version               print program version\n'
-          '\n\n'
-          'Default rsync options: %s\n'
-          '\n'
-          'Written by Laurence Liu <liuxy6@gmail.com>'\
-                  % (__NAME__, __VERSION__, __NAME__, BACKUP.default_options))
+    print(
+        '%s %s, Use rsync to backup and to restore files.\n'
+        'Usage: %s [OPTIONS...] CONFIG_FILE [ADDITIONAL_RSYNC_OPTIONS...]\n'
+        '\n'
+        'OPTIONS:\n'
+        '\n'
+        '  Informative Output:\n'
+        '      -q, --quiet                 keep quiet\n'
+        '      -v, --verbose               increase verbosity\n'
+        '\n'
+        '  Backup Options:\n'
+        '          --rsnyc-opts="..."      replace the default rsync options\n'
+        '      -n, --show-cmd              print rsync command and exit\n'
+        '\n'
+        '  Other Options:\n'
+        '      -h, --help                  print this help list\n'
+        '      -V, --version               print program version\n'
+        '\n\n'
+        'Default rsync options: %s\n'
+        '\n'
+        'Written by Laurence Liu <liuxy6@gmail.com>'
+        % (__NAME__, __VERSION__, __NAME__, DEFAULT_OPTIONS))
 
 
 def print_version():
-    print('%s %s\n'
-          'Copyright (C) 2014-2016  Laurence Liu <liuxy6@gmail.com>\n'
-          'License GPL v3: GNU GPL version 3 <http://www.gnu.org/licenses/>\n'
-          'This program comes with ABSOLUTELY NO WARRANTY.\n'
-          'This is free software, and you are welcome to redistribute it.\n'
-          '\n'
-          'Written by Laurence Liu <liuxy6@gmail.com>' %
-          (__NAME__, __VERSION__))
+    print(
+        '%s %s\n'
+        'Copyright (C) 2014-2016  Laurence Liu <liuxy6@gmail.com>\n'
+        'License GPL v3: GNU GPL version 3 <http://www.gnu.org/licenses/>\n'
+        'This program comes with ABSOLUTELY NO WARRANTY.\n'
+        'This is free software, and you are welcome to redistribute it.\n'
+        '\n'
+        'Written by Laurence Liu <liuxy6@gmail.com>'
+        % (__NAME__, __VERSION__))
 
 
 def get_conf(filepath, config=None):
@@ -84,8 +100,6 @@ def get_conf(filepath, config=None):
 
 
 class BACKUP(object):
-    default_options = '--verbose --human-readable --archive --hard-links --acls --xattrs --numeric-ids --noatime --inplace --delete --delete-excluded'
-
     def __init__(self, rsync_opts=''):
         self.__ori_dir = self.__des_dir = self.__include = self.__exclude = self.__options = ''
         self.add_options(rsync_opts)
@@ -122,13 +136,13 @@ class BACKUP(object):
     def gen_cmd(self):
         include = '--include-from="%s"' % self.__include if self.__include else ''
         exclude = '--exclude-from="%s"' % self.__exclude if self.__exclude else ''
-        return 'rsync %s %s %s %s "%s" "%s"' %\
-                (self.default_options,\
-                        self.__options,\
-                        include,\
-                        exclude,\
-                        self.__ori_dir,\
-                        self.__des_dir)
+        return 'rsync %s %s %s %s "%s" "%s"' % (
+            DEFAULT_OPTIONS,
+            self.__options,
+            include,
+            exclude,
+            self.__ori_dir,
+            self.__des_dir)
 
     def run(self):
         start = int(time())
@@ -163,20 +177,21 @@ def main():
     show_cmd = False
 
     try:
-        opts, args = getopt.getopt(argv, 'qv nhV',\
-                ['quiet', 'verbose', 'rsnyc-opts=', 'show-cmd', 'help', 'version'])
+        opts, args = getopt.getopt(argv, 'qv nhV',
+                                   ['quiet', 'verbose', 'rsnyc-opts=', 'show-cmd', 'help', 'version'])
     except getopt.GetoptError as error:
         logger.critical(error)
         exit()
+
+    global DEFAULT_OPTIONS
     for o, a in opts:
         if o in ('-q', '--quiet'):
             logger.setLevel(logging.WARN)
-            BACKUP.default_options = BACKUP.default_options.replace('--verbose',
-                                                                    '')
+            DEFAULT_OPTIONS = DEFAULT_OPTIONS.replace('--verbose', '')
         elif o in ('-v', '--verbose'):
             logger.setLevel(logging.DEBUG)
         elif o in ('--rsnyc-opts',):
-            BACKUP.default_options = a
+            DEFAULT_OPTIONS = a
             logger.debug('Set default options: %s', a)
         elif o in ('-n', '--show-cmd'):
             show_cmd = True
